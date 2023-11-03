2136c16d5c5ae642ddaba219d0b964087f0e65b2
amina <amina.elkabli1@gmail.com>
2023-11-03 12:00:12 +0100

    add departements and employes

--- src/main/java/main/MainApp.java
+++ src/main/java/main/MainApp.java
@@ -10,6 +10,7 @@ import service.DepService;
 import service.EmployeService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Controller
@@ -20,28 +21,36 @@ public class MainApp {
         EmployeService employeService = context.getBean("employeService", EmployeService.class);
 
         // add département
-      //  Département dep1= new Département("RH");
+     // Département dep1= new Département("RH");
+       //Département dep2= new Département("Marketing");
+      // Département dep3= new Département("finance");
+      // Département dep4= new Département("vente");
 
-      //  depService.ajouterDepartement(dep1);
+          // depService.ajouterDepartement(dep2);
+         //  depService.ajouterDepartement(dep3);
+         //  depService.ajouterDepartement(dep4);
+
 
         // Créer un employé
-      //  Employé employe = new Employé("amina", 12345, dep1);
-       // employeService.save(employe);
+       // Employé employe1 = new Employé("Faiza", 2000, dep2);
+       // Employé employe2 = new Employé("Sofia", 20000, dep3);
+       // Employé employe3 = new Employé("Simo", 15000, dep4);
+        //  employeService.save(employe1);
+        //  employeService.save(employe2);
+        //  employeService.save(employe3);
 
         //récupérer tous les employés
-       /* List<Employé> allEmployees =employeService.getAllEmploye();
-        for (Employé emp : allEmployees) {
-            System.out.println(emp);
-        }
+        List<Employé> allEmployees =employeService.getAllEmploye();
+        allEmployees.stream().forEach(System.out::println);
 
-        */
+      //  }
 
         // Récupérer un employé spécifique par son ID
-           Employé employeToUpdate = employeService.getEmployeById(1L);
+         /* Employé employeToUpdate = employeService.getEmployeById(1L);
            employeToUpdate.setName("Jodia");
            employeToUpdate.setSalaire(10000);
            employeService.updateEmploye(employeToUpdate);
 
-
+       */
     }
 }
